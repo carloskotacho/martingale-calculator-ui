dd503e62cf74935948e6ad1cebb4b9e850a85983
Carlos K <carloskotachoamd@hotmail.com>
2019-12-27 18:08:12 -0300

    Adicionado style form

--- src/app/martingale/martingale.module.ts
+++ src/app/martingale/martingale.module.ts
@@ -2,9 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MartingaleCalculatorComponent } from './martingale-calculator/martingale-calculator.component';
 
+import { PanelModule } from 'primeng/panel';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    PanelModule,
+    InputTextModule,
+    ButtonModule,
+    CardModule,
+    ScrollPanelModule
   ],
   declarations: [
     MartingaleCalculatorComponent
